8dece5af7999289ac5894578443978dbe11730d7
Alexey Pavlov <laraziel@yandex.ru>
2021-11-30 13:04:36 +0300

    Initial Commit

---
+++ AlbumSeeker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlbumSeeker
+//
+//  Created by user on 30.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
